964db4b90ae48a7032073c008740027c1d3e2557
njshouse <njshouse@oakland.edu>
2016-02-03 14:58:49 -0500

    Added main program .idea and src

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 99;
+        if (i!=0) {
+            while (i != 1) {
+                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer take one down, pass it around, ");
+                i--;
+                if(i!=1) {
+                    System.out.println(i + " bottles of beer on the wall.");
+                }
+                else{
+                    System.out.println("1 bottle of beer on the wall.");
+                    System.out.println("1 bottle of beer on the wall, 1 bottle of beer, take one down, pass it around, no more bottles of beer on the wall.");
+                }
+
+            }
+        }
+        }
+    }
