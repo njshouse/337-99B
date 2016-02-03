8f6ab7601536f203513a83c35c560561ad767798
njshouse <njshouse@oakland.edu>
2016-02-03 15:23:48 -0500

    Added 99 bottles function to code

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,7 +1,11 @@
 public class BeerSong {
 
     public static void main(String[] args) {
-        int i = 99;
+        int i =99;
+        Ninety_Nine_Bottles_of_Beer(i);
+
+        }
+    public static int Ninety_Nine_Bottles_of_Beer(int i){
         if (i!=0) {
             while (i != 1) {
                 System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer take one down, pass it around, ");
@@ -16,5 +20,6 @@ public class BeerSong {
 
             }
         }
-        }
+        return i;
+    }
     }
